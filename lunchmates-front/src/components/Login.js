7c1051e7a84b9aea90b6995ca5f8f6f3ad9f0eb8
Adèle Revert <revertadele@hotmail.fr>
2018-12-07 12:40:15 +0100

    created the forms for signup and login

--- lunchmates-front/src/components/Login.js
+++ lunchmates-front/src/components/Login.js
@@ -1,18 +1,61 @@
 import React, { Component } from "react";
+import axios from "axios";
+import { Redirect } from "react-router-dom";
 
 class Login extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      email: "",
+      originalPassword: "",
+    };
   }
+
+  genericSync(event){
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  }
+
+  handleSubmit(event){
+    event.preventDefault();
+
+    axios
+    .post("http://localhost:5555/login", this.state, {withCredentials: true})
+    .then(response => {
+      console.log("Login Page", response.data);
+      const { userDoc } = response.data;
+      this.props.onUserChange(userDoc);
+    })
+    .catch(err => {
+      console.log("Login Page ERROR", err);
+      alert("Sorry! Something went wrong");
+    });
+  }
+  
   render() {
+    if(this.props.currentUser){
+      return <Redirect to="/hompage" />
+    }
     return (
       <section className="Login">
         <h2>Log In</h2>
 
-        <form>
-          <input type="email" name="email" placeholder="Email" />
-          <input type="password" name="originalPassword" placeholder="*****" />
+        <form onSubmit={event => this.handleSubmit(event)}>
+          <input 
+          value={this.state.email}
+          onChange={event => this.genericSync(event)}
+          type="email" 
+          name="email" 
+          placeholder="Email" 
+          />
+          <input 
+          value={this.state.originalPassword}
+          onChange={event => this.genericSync(event)}
+          type="password" 
+          name="originalPassword" 
+          placeholder="*****" 
+          />
+
         </form>
 
         <button>Log In</button>
--- lunchmates-front/src/components/Signup.js
+++ lunchmates-front/src/components/Signup.js
@@ -1,22 +1,96 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class Signup extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      firstName: "",
+      lastName: "",
+      email: "",
+      companyName: "",
+      originalPassword: "",
+      messenger: false,
+      currentUser: null,
+    };
   }
+
+  genericSync(event){
+    cont { name, value } = event.target;
+    this.setState({ [name]: value });
+  }
+
+  handleSubmitEvent(event){
+    event.preventDefault();
+
+    axios
+    .post("http://localhost:5555/signup", this.state, {withCredentials: true })
+    .then(response => {
+      console.log("Signup Page", response.data);
+      const { userDoc } = response.data;
+      this.props.onUserChange(userDoc);
+    })
+    .catch(err => {
+      console.log("SIgnup Page ERROR", err);
+      alert("SOmething went wrong");
+    });
+  }
+
   render() {
     return (
       <section className="Signup">
         <h2>Sign Up</h2>
 
-        <form>
-          <input type="text" name="firstName" placeholder="First Name" />
-          <input type="text" name="lastName" placeholder="Last Name" />
-          <input type="email" name="email" placeholder="Email" />
-          <input type="password" name="originalPassword" placeholder="*****" />
-          <input type="text" name="companyName" placeholder="Company Name" />
-          <input type="checkbox" name="messenger" />Do you allow other lunchmates to you you to go to lunch?
+        <form onSubmit={event => this.handleSubmitEvent(event)}>
+
+          <input
+          value={this.state.firstName} 
+          onChange={event => this.genericSync(event)}
+          type="text" 
+          name="firstName" 
+          placeholder="First Name" 
+          />
+
+          <input
+          value={this.state.lastName}
+          onChange={event => this.genericSync(event)} 
+          type="text" 
+          name="lastName" 
+          placeholder="Last Name" 
+          />
+
+          <input
+          value={this.state.email}
+          onChange={event => this.genericSync(event)}
+          type="email" 
+          name="email" 
+          placeholder="Email" 
+          />
+
+          <input 
+          value={this.state.originalPassword}
+          onChange={event => this.genericSync(event)}
+          type="password" 
+          name="originalPassword" 
+          placeholder="*****" 
+          />
+
+          <input 
+          value={this.state.companyName}
+          onChange={event => this.genericSync(event)}
+          type="text" 
+          name="companyName" 
+          placeholder="Company Name" 
+          />
+
+          <input 
+          value={this.state.messenger}
+          onChange={event => this.genericSync(event)}
+          type="checkbox" 
+          name="messenger" 
+          />
+          Do you allow other lunchmates to you you to go to lunch?
+        
         </form>
 
         <button>Sign Up</button>
