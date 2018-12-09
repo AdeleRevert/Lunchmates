85d299ace806832f5df16db21459ba206da0b40e
Adèle Revert <revertadele@hotmail.fr>
2018-12-09 17:18:47 +0100

    css and responsive on RestaurantPicturePreview, responsive on the homepage

--- lunchmates-front/src/components/RestaurantPicturePreview.js
+++ lunchmates-front/src/components/RestaurantPicturePreview.js
@@ -1,17 +1,51 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import "./RestaurantPicturePreview.css";
 
 class RestaurantPicturePreview extends Component {
   constructor(props) {
     super(props);
-    this.state = {  }
+    this.state = {
+      searchResults: []
+    };
   }
-  render() { 
-    return ( 
+
+  componentDidMount() {
+    axios
+      .get(`http://localhost:5000/api/shop/`, { withCredentials: true })
+      .then(response => {
+        console.log("Response data of shop/searchTerm:", response.data);
+        this.setState({ searchResults: response.data.businesses });
+      })
+      .catch(err => {
+        console.log("Search page ERROR", err);
+        alert("Something went wrong with the search, sorray");
+      });
+  }
+
+  render() {
+    const { searchResults } = this.state;
+    searchResults.splice(4);
+    return (
       <section className="RestaurantPicturePreview">
-      <h2>Hola soy tu RestaurantPicturePreview Component</h2>
+        <ul className="ListofRestaurantPicturePreview">
+          {searchResults.map(oneResult => {
+            return (
+              <li key={oneResult.id} className="OneRestaurantPicturePreview">
+                <Link to={`/shop-details/${oneResult.id}`}><img src={oneResult.image_url} alt="restaurant"/></Link>
+                <div className="RestaurantInfoOverlay">
+                <p>{oneResult.name}</p>
+                <p>{oneResult.location.address1} {oneResult.location.zip_code} {oneResult.location.city}</p>
+                <p>{oneResult.rating}</p>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
       </section>
-     );
+    );
   }
 }
- 
+
 export default RestaurantPicturePreview;
